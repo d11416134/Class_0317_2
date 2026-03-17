105981d48d1343f3fabbb8a60f2f3050b42ac431
d11416134 <d11416134@gs.takming.edu.tw>
2026-03-17 15:43:19 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Class_0317_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Test std1, std2;
+           
+            std1 = new Test();
+            std2 = new Test();
+
+            std1.Mid = 78;
+            std1.SetFinal(72);
+
+            std2.SetGrade(68, 84);
+
+            Console.WriteLine(std1.GetAvg().ToString());
+
+            Console.WriteLine(std2.GetAvg().ToString());
+            
+        }
+    }
+}
